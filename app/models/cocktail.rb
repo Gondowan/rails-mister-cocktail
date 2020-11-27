d1169316fda242c0dd662e7e7dbfb9f29cd316f4
Adrian Diaz <soulspire.fc@gmail.com>
2020-11-27 16:30:41 +0100

    Almost final version of Cocktails

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
-  mount_uploader :photo, PhotoUploader
+  has_one_attached :photo
 
   validates :name, uniqueness: true, presence: true
 end
